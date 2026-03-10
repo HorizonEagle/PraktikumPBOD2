d1b97e79a6dfd8d766d45eb865ac2816d7d78e3d
Ali Maskan Ferry Purwanto <alimaskanferry845@gmail.com>
2026-03-10 13:35:36 +0700

    Test

---
+++ Pertemuan4/src/BangunDatar.java
@@ -0,0 +1,35 @@
+public class BangunDatar {
+private int jmlsisi;
+private String warna;
+private String border;
+
+public BangunDatar () {}
+
+public int getJmlSisi () {
+        return jmlsisi;
+}
+
+public void setJmlSisi (int jmlSisi) {
+this.jmlsisi= jmlSisi;
+}
+public String getWarna () {
+    return warna;
+}
+public void setWarna (String warna) {
+    this.warna = warna;
+}
+
+public String getBorder () {
+return border;
+}
+public void setBorder (String border) {
+this.border = border;
+}
+
+public void printInfo () {
+System. out.println ("Jumlah sisi: " + jmlsisi) ;
+System. out.println ("Warna: " + warna) ;
+System. out.println ("Border: " + border) ;
+}
+
+}
---
+++ Pertemuan4/src/MainBangunDatar.java
@@ -0,0 +1,10 @@
+public class MainBangunDatar {
+    
+    public static void main(String[] args) {
+        Persegi kotak = new Persegi();
+
+        kotak.setSisi(5);
+
+        kotak.printInfo();
+    }
+}
