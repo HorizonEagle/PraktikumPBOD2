58ce9f1b4c4c01bfddec77a670f579a69daa8f4b
Ali Maskan Ferry Purwanto <alimaskanferry845@gmail.com>
2026-03-10 14:50:27 +0700

    Bangun Datar Final

---
+++ Pertemuan4/src/Pegawai.java
@@ -0,0 +1,16 @@
+import java.security.PublicKey;
+import java.util.Date;
+
+public class Pegawai {
+    private String NIP;
+    private String Nama;
+    private Date TanggalLahir;
+    private Date TMT;
+    private double Gaji;
+
+    public Pegawai(){}
+    public Pegawai(String NIP,String Nama,Date TanggalLahir,Date TMT,double Gaji){
+    }
+
+
+}
