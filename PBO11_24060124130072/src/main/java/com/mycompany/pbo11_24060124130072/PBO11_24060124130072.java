033ecb2753d83aa66e7ecd25beb39ac9da83e526
Ali Maskan Ferry Purwanto <alimaskanferry845@gmail.com>
2026-05-24 22:02:24 +0700

    Pertemuan 11

---
+++ PBO11_24060124130072/src/main/java/com/mycompany/pbo11_24060124130072/PBO11_24060124130072.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.pbo11_24060124130072;
+
+/**
+ *
+ * @author ferry
+ */
+public class PBO11_24060124130072 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
